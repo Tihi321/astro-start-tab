844257632a8bee51d8ed26eb0cb8d4f771cac779
Tihomir Selak <tknox.dr@gmail.com>
2024-03-15 12:43:25 +0100

    SP-03: Add toggle custom

--- src/blocks/shortcuts/CustomShortcuts.tsx
+++ src/blocks/shortcuts/CustomShortcuts.tsx
@@ -4,8 +4,15 @@ import { styled } from "solid-styled-components";
 
 const Container = styled("div")`
   position: relative;
-  height: 160px;
   padding: 8px 0;
+`;
+
+const ShowContainer = styled("div")`
+  height: 160px;
+`;
+
+const HiddenContainer = styled("div")`
+  height: 40px;
 `;
 
 const Shortcuts = styled("div")`
@@ -69,16 +76,58 @@ const AddContainer = styled("div")`
   height: 100%;
 `;
 
-const AddButton = styled("button")`
-  width: 100%;
+const ToggleButtons = styled("div")`
+  display: flex;
+  flex-direction: row;
   height: 20px;
+  width: 100%;
+`;
+
+const ShowShortcutsButton = styled("button")`
   padding: 4px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 200px;
+  height: 20px;
+  position: absolute;
+  top: -20px;
+  left: 50%;
+  transform: translateX(-50%);
   line-height: 1;
   border-radius: 4px;
   background: var(--dark);
   color: var(--text);
   border: 1px solid var(--light);
   cursor: pointer;
+`;
+
+const HideShortcutsButton = styled("button")`
+  padding: 4px;
+  flex: 1;
+  line-height: 1;
+  border-radius: 4px;
+  background: var(--dark);
+  color: var(--text);
+  border: 1px solid var(--light);
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const AddButton = styled("button")`
+  padding: 4px;
+  flex: 1;
+  line-height: 1;
+  border-radius: 4px;
+  background: var(--dark);
+  color: var(--text);
+  border: 1px solid var(--light);
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Inputs = styled("div")`
@@ -125,11 +174,18 @@ const SaveButton = styled("button")`
 
 export const CustomShortcuts = () => {
   const [shortcuts, setShortcuts] = createSignal<Array<{ name: string; url: string }>>([]);
+  const [showCustomShortcuts, setShowCustomShotcuts] = createSignal<boolean>(false);
   const [showInputs, setShowInputs] = createSignal<boolean>(false);
   const [name, setName] = createSignal<string>("");
   const [url, setUrl] = createSignal<string>("");
 
   onMount(() => {
+    const showCustomShortcuts = localStorage.getItem("showcustomshortcuts") || "false";
+
+    if (showCustomShortcuts === "true") {
+      setShowCustomShotcuts(true);
+    }
+
     const shortcuts = localStorage.getItem("shortcuts")
       ? JSON.parse(localStorage.getItem("shortcuts") || "")
       : [];
@@ -138,57 +194,83 @@ export const CustomShortcuts = () => {
 
   return (
     <Container>
-      <Shortcuts>
-        {map(shortcuts(), (values) => (
-          <Shortcut>
-            <Link href={values.url}>{values.name}</Link>
-            <RemoveButton
-              onClick={() => {
-                const newShortcuts = shortcuts().filter(
-                  (shortcut) => !isEqual(shortcut.name, values.name)
-                );
-                setShortcuts(newShortcuts);
-                localStorage.setItem("shortcuts", JSON.stringify(newShortcuts));
-              }}
-            >
-              -
-            </RemoveButton>
-          </Shortcut>
-        ))}
-      </Shortcuts>
-      <AddContainer>
-        <AddButton onClick={() => setShowInputs(!showInputs())}>
-          {showInputs() ? "-" : "+"}
-        </AddButton>
-        <Inputs class={showInputs() ? "show" : ""}>
-          <Input
-            autocomplete="off"
-            type="text"
-            value={name()}
-            onInput={(e: any) => setName(e.target.value)}
-            placeholder="Name"
-          />
-          <Input
-            autocomplete="off"
-            type="text"
-            value={url()}
-            onInput={(e: any) => setUrl(e.target.value)}
-            placeholder="URL"
-          />
-          <SaveButton
+      {!showCustomShortcuts() && (
+        <HiddenContainer>
+          <ShowShortcutsButton
             onClick={() => {
-              const newShortcuts = [...shortcuts(), { name: name(), url: url() }];
-              setShortcuts(newShortcuts);
-              localStorage.setItem("shortcuts", JSON.stringify(newShortcuts));
-              setName("");
-              setUrl("");
-              setShowInputs(false);
+              localStorage.setItem("showcustomshortcuts", "true");
+              setShowCustomShotcuts(true);
             }}
           >
-            Save
-          </SaveButton>
-        </Inputs>
-      </AddContainer>
+            {"show"}
+          </ShowShortcutsButton>
+        </HiddenContainer>
+      )}
+      {showCustomShortcuts() && (
+        <ShowContainer>
+          <Shortcuts>
+            {map(shortcuts(), (values) => (
+              <Shortcut>
+                <Link href={values.url}>{values.name}</Link>
+                <RemoveButton
+                  onClick={() => {
+                    const newShortcuts = shortcuts().filter(
+                      (shortcut) => !isEqual(shortcut.name, values.name)
+                    );
+                    setShortcuts(newShortcuts);
+                    localStorage.setItem("shortcuts", JSON.stringify(newShortcuts));
+                  }}
+                >
+                  -
+                </RemoveButton>
+              </Shortcut>
+            ))}
+          </Shortcuts>
+          <AddContainer>
+            <ToggleButtons>
+              <AddButton onClick={() => setShowInputs(!showInputs())}>
+                {showInputs() ? "-" : "+"}
+              </AddButton>
+              <HideShortcutsButton
+                onClick={() => {
+                  localStorage.setItem("showcustomshortcuts", "false");
+                  setShowCustomShotcuts(false);
+                }}
+              >
+                {"hide"}
+              </HideShortcutsButton>
+            </ToggleButtons>
+            <Inputs class={showInputs() ? "show" : ""}>
+              <Input
+                autocomplete="off"
+                type="text"
+                value={name()}
+                onInput={(e: any) => setName(e.target.value)}
+                placeholder="Name"
+              />
+              <Input
+                autocomplete="off"
+                type="text"
+                value={url()}
+                onInput={(e: any) => setUrl(e.target.value)}
+                placeholder="URL"
+              />
+              <SaveButton
+                onClick={() => {
+                  const newShortcuts = [...shortcuts(), { name: name(), url: url() }];
+                  setShortcuts(newShortcuts);
+                  localStorage.setItem("shortcuts", JSON.stringify(newShortcuts));
+                  setName("");
+                  setUrl("");
+                  setShowInputs(false);
+                }}
+              >
+                Save
+              </SaveButton>
+            </Inputs>
+          </AddContainer>
+        </ShowContainer>
+      )}
     </Container>
   );
 };
